d54fb7ef106a39fb500216414dc09a28c009d0e7
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-11 18:40:00 -0400

    Add post_id to Comment migration

--- db/migrate/20180911223747_create_comments.rb
+++ db/migrate/20180911223747_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.0]
     create_table :comments do |t|
       t.string :author
       t.string :content
+      t.integer :post_id
 
       t.timestamps
     end
