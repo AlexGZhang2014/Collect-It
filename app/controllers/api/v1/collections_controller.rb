a575eeb5022027407df3406f90a27b1e1e041804
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-11 19:38:08 -0400

    Add standard JSON-rendering CRUD actions for all controllers

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -1,7 +1,60 @@
 module Api::V1
 
 class CollectionsController < ApplicationController
-  
+  before_action :set_collection, only: [:show, :update, :destroy]
+
+  # GET /collections
+  def index
+    @collections = Collection.order(:id)
+
+    render json: @collections
+  end
+
+  # GET /collections/1
+  def show
+    render json: @collection
+  end
+
+  # POST /collections
+  def create
+    @collection = Collection.new(collection_params)
+
+    if @collection.save
+      render json: @collection, status: :created
+    else
+      render json: @collection.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /collections/1
+  def update
+    if @collection.update(collection_params)
+      render json: @collection
+    else
+      render json: @collection.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /collections/1
+  def destroy
+    @collection.destroy
+    if @collection.destroy
+      head :no_content, status: :ok
+    else
+      render json: @collection.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_collection
+      @collection = Collection.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white collection" through.
+    def collection_params
+      params.require(:collection).permit(:name, :description, :owner)
+    end
 end
 
 end
--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,60 @@
 module Api::V1
 
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :update, :destroy]
+
+  # GET /items
+  def index
+    @items = Item.order(:id)
+
+    render json: @items
+  end
+
+  # GET /items/1
+  def show
+    render json: @item
+  end
+
+  # POST /items
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      render json: @item, status: :created
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /items/1
+  def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /items/1
+  def destroy
+    @item.destroy
+    if @item.destroy
+      head :no_content, status: :ok
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white item" through.
+    def item_params
+      params.require(:item).permit(:name, :description, :collection_id)
+    end
 end
 
 end
