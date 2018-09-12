2c151cca49d592f17efcfef1bad9cd4ae126ce54
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 18:11:00 -0400

    Pass down toggleEditOn() as props

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -2,38 +2,21 @@ import React, { Component } from 'react'
 import CommentsContainer from '../../containers/CommentsContainer'
 import Moment from 'react-moment'
 import 'moment-timezone'
-import EditPostForm from './EditPostForm'
 
 class Post extends Component {
-  state = {
-    editPostStatus: false
-  }
-
-  editPost = () => {
-    this.setState({
-      editPostStatus: true
-    })
-  }
-
   render() {
     const post = this.props.post
 
-    if (this.state.editPostStatus) {
-      return (
-        <EditPostForm post={post} updatePost={this.props.updatePost}/>
-      );
-    } else {
-      return (
-        <div className="post">
-          <h2>{post.title}</h2>
-          <h4>Written by: {post.author} (<Moment date={post.created_at} fromNow />)</h4>
-          <p>{post.content}</p>
-          <button onClick={() => this.editPost(post.id)}>Edit this post</button>
-          <button onClick={() => this.props.deletePost(post.id)}>Delete this post</button>
-          <CommentsContainer post={post}/>
-        </div>
-      );
-    }
+    return (
+      <div className="post">
+        <h2>{post.title}</h2>
+        <h4>Written by: {post.author} (<Moment date={post.created_at} fromNow />)</h4>
+        <p>{post.content}</p>
+        <button onClick={() => this.props.toggleEditOn(post.id)}>Edit this post</button>
+        <button onClick={() => this.props.deletePost(post.id)}>Delete this post</button>
+        <CommentsContainer post={post}/>
+      </div>
+    );
   }
 }
 
