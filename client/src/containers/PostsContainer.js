895943969c3af8d26e64946292024a16a24dfe57
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-11 20:42:26 -0400

    Modify proxy so that fetching works and map all posts in PostsContainer for now

--- client/src/containers/PostsContainer.js
+++ client/src/containers/PostsContainer.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 
 class PostsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/v1/posts.json')
+      .then(response => response.json())
+      .then(postData => this.setState({
+        posts: postData
+      }))
+  }
+
   render() {
     return (
       <div className="posts-container">
-        Posts
+        {this.state.posts.map(post => {
+          return (
+            <div className="post" key={post.id}>
+              <h3>{post.title}</h3>
+              <h4>Written by: {post.author}</h4>
+              <p>{post.content}</p>
+            </div>
+          )
+        })}
       </div>
     )
   }
