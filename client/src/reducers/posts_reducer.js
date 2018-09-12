b9d106771394dc1b5cd0217f1ab84fdb44836261
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 13:14:43 -0400

    Define case for UPDATED_POST action

--- client/src/reducers/posts_reducer.js
+++ client/src/reducers/posts_reducer.js
@@ -1,7 +1,6 @@
 export default function postsReducer(state = {
   loading: false,
-  posts: [],
-  comments: []
+  posts: []
 }, action) {
   switch (action.type) {
     case 'LOADING_POSTS':
@@ -11,14 +10,29 @@ export default function postsReducer(state = {
       return { loading: false, posts: action.payload }
 
     case 'ADDED_POST':
-      const post = { id: action.id, title: action.title, content: action.content, author: action.author, comments: [] }
-      return { ...state, posts: [...state.posts, post]}
+      const post = { id: action.id, title: action.title, content: action.content, author: action.author, comments: [] };
+
+      return {
+        ...state,
+        posts: [...state.posts, post]
+      }
 
     case 'DELETED_POST':
       return {
         ...state,
         posts: state.posts.filter(post => post.id !== action.id)
       }
+
+    case 'UPDATED_POST':
+      let newState = {
+        ...state,
+        posts: state.posts.filter(post => post.id !== action.id)
+      }
+
+      const editedPost = { id: action.id, title: action.title, content: action.content, author: action.author, comments: [] };
+
+      newState.posts.concat(editedPost);
+      return newState;
 
     default:
       return state;
