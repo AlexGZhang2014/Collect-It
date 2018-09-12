bb4604ddd6e079ddf152ea64d55d1c0ccad42ebf
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 01:55:01 -0400

    Remove connect for now

--- client/src/containers/CommentsContainer.js
+++ client/src/containers/CommentsContainer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 import Comments from '../components/comments/Comments'
 
 class CommentsContainer extends Component {
