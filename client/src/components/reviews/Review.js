5bb0c12bc797a281fd4dc8c57defa259a6a183a7
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 02:52:18 -0400

    Add logic to display STARS only when rating > 1

--- client/src/components/reviews/Review.js
+++ client/src/components/reviews/Review.js
@@ -3,10 +3,12 @@ import Moment from 'react-moment'
 import 'moment-timezone'
 
 const Review = props => {
+  const stars = props.review.rating > 1 ? "STARS" : "STAR"
+
   return (
     <div className="review">
       <h4>{props.review.author} wrote: (<Moment date={props.review.created_at} fromNow />)</h4>
-      <p><strong>{props.review.rating} STARS</strong></p>
+      <p><strong>{props.review.rating} {stars}</strong></p>
       <p>{props.review.content}</p>
     </div>
   )
