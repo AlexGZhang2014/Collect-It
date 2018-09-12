c7b1616a5be0f18ab05382fa01e59e0d96513ebe
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 13:54:08 -0400

    Refactor Post Component to have state and render EditPostForm component

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -2,20 +2,38 @@ import React, { Component } from 'react'
 import CommentsContainer from '../../containers/CommentsContainer'
 import Moment from 'react-moment'
 import 'moment-timezone'
+import EditPostForm from './EditPostForm'
 
 class Post extends Component {
+  state = {
+    editPostStatus: false
+  }
+
+  editPost = () => {
+    this.setState({
+      editPostStatus: true
+    })
+  }
+
   render() {
     const post = this.props.post
 
-    return (
-      <div className="post">
-        <h2>{post.title}</h2>
-        <h4>Written by: {post.author} (<Moment date={post.created_at} fromNow />)</h4>
-        <p>{post.content}</p>
-        <button onClick={() => this.props.deletePost(post.id)}>X</button>
-        <CommentsContainer post={post}/>
-      </div>
-    )
+    if (this.state.editPostStatus) {
+      return (
+        <EditPostForm post={post} updatePost={this.props.updatePost}/>
+      );
+    } else {
+      return (
+        <div className="post">
+          <h2>{post.title}</h2>
+          <h4>Written by: {post.author} (<Moment date={post.created_at} fromNow />)</h4>
+          <p>{post.content}</p>
+          <button onClick={this.editPost}>Edit this post</button>
+          <button onClick={() => this.props.deletePost(post.id)}>Delete this post</button>
+          <CommentsContainer post={post}/>
+        </div>
+      );
+    }
   }
 }
 
