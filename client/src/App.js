72d8a07879679eab13dc791fa9e92b5d9d656cac
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-11 21:29:19 -0400

    Establish React Router in App.js to render Home component and also changed NavBar component styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 import PostsContainer from './containers/PostsContainer'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Collect-It!</h1>
-        </header>
-        <PostsContainer />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to Collect-It!</h1>
+          </header>
+          <NavBar />
+          <Route exact path="/" component={Home} />
+        </div>
+      </Router>
     );
   }
 }
