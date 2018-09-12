1019b97a1b02ef72b1eb335111d6ad075fc55d5e
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 13:55:46 -0400

    Create EditPostForm component and confirm that updating posts is working

---
+++ client/src/components/posts/EditPostForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+class EditPostForm extends Component {
+  state = {
+    id: this.props.post.id,
+    title: this.props.post.title,
+    content: this.props.post.content,
+    author: this.props.post.author
+  }
+
+  handleChange = event => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.updatePost(this.state);
+    this.setState({
+      id: this.props.post.id,
+      title: '',
+      content: '',
+      author: ''
+    });
+  }
+
+  render() {
+    return (
+      <div className="post-form" onSubmit={this.handleSubmit}>
+        <form>
+          <h2>Edit your post here!</h2>
+          <br />
+          <label>Your Post Title:</label>
+          <br />
+          <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
+          <br />
+          <label>Your Name:</label>
+          <br />
+          <input type="text" name="author" value={this.state.author} onChange={this.handleChange} />
+          <br />
+          <label>Your Post Content:</label>
+          <br />
+          <textarea name="content" value={this.state.content} onChange={this.handleChange} />
+          <br />
+          <input type="submit" value="Update post"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default EditPostForm
