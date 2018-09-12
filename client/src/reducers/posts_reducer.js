694890f0a09704ee0451d6127fe7e26cc41cef22
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 11:22:37 -0400

    Change case ADD_POST to ADDED_POST

--- client/src/reducers/posts_reducer.js
+++ client/src/reducers/posts_reducer.js
@@ -13,7 +13,7 @@ export default function postsReducer(state = {
     case 'FETCH_POSTS':
       return { loading: false, posts: action.payload }
 
-    case 'ADD_POST':
+    case 'ADDED_POST':
       const post = { id: cuidFn(), title: action.title, content: action.content, author: action.author, comments: [] }
       return { ...state, posts: [...state.posts, post]}
 
