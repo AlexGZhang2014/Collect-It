7ad8c460360bee6948fcb4310102e1dcd9eadfe6
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 18:12:14 -0400

    Refactor so that Posts either render as the Edit form or as Post components depending on toggle functions

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,12 +3,49 @@ import Post from './Post'
 import EditPostForm from './EditPostForm'
 
 class Posts extends Component {
-  render() {
-    const posts = this.props.posts.map(post => <Post key={post.id} post={post} deletePost={this.props.deletePost} updatePost={this.props.updatePost} />)
+  constructor(props) {
+    super(props);
+    this.state = {
+      editPostId: null
+    }
+  }
 
+  toggleEditOn = id => {
+    this.setState({
+      editPostId: id
+    })
+  }
+
+  toggleEditOff = () => {
+    this.setState({
+      editPostId: null
+    })
+  }
+
+  render() {
     return (
       <div className="posts">
-        {posts}
+        {this.props.posts.map(post => {
+          if (this.state.editPostId === post.id) {
+            return (
+              <EditPostForm
+                post={post}
+                key={post.id}
+                updatePost={this.props.updatePost}
+                toggleEditOff={this.toggleEditOff}
+                />
+            )
+          } else {
+            return (
+              <Post
+                key={post.id}
+                post={post}
+                deletePost={this.props.deletePost}
+                toggleEditOn={this.toggleEditOn}
+                />
+            )
+          }
+        })}
       </div>
     )
   }
