49a30c8eb8980f7c25b9040e904038b01331d735
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 11:21:33 -0400

    Move addPost to postActions.js and redefine it as a fetch action of type POST to allow persistence of data

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,8 +1,35 @@
-export default function fetchPosts() {
+export function fetchPosts() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_POSTS' });
     return fetch('/api/v1/posts.json')
       .then(response => response.json())
       .then(postData => dispatch({ type: 'FETCH_POSTS', payload: postData }));
+  }
+}
+
+export function addPost(state) {
+  return dispatch => {
+    dispatch({ type: 'ADDING_POST' });
+
+    const postData = {
+      post: {
+        title: state.title,
+        content: state.content,
+        author: state.author
+      }
+    }
+
+    fetch('/api/v1/posts', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(postData)
+    })
+      .then(response => response.json())
+      .then(postJSON => dispatch({
+        type: "ADDED_POST",
+        title: postJSON.title,
+        content: postJSON.content,
+        author: postJSON.author
+      }));
   }
 }
