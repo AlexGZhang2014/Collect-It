caf715aef849b2ea224e6353608f2aa6933e4298
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 13:15:25 -0400

    Import updatePost and add the function to mapDispatchToProps

--- client/src/containers/PostsContainer.js
+++ client/src/containers/PostsContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Posts from '../components/posts/Posts'
-import { fetchPosts, addPost, deletePost } from '../actions/postActions'
+import { fetchPosts, addPost, deletePost, updatePost } from '../actions/postActions'
 import PostInput from '../components/posts/PostInput'
 
 class PostsContainer extends Component {
@@ -15,7 +15,7 @@ class PostsContainer extends Component {
       <div className="posts-container">
         <h1>Your Posts Feed!</h1>
         <PostInput addPost={this.props.addPost}/>
-        <Posts posts={this.props.posts} deletePost={this.props.deletePost}/>
+        <Posts posts={this.props.posts} deletePost={this.props.deletePost} updatePost={this.props.updatePost}/>
       </div>
     )
   }
@@ -28,7 +28,8 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   fetchPosts: () => dispatch(fetchPosts()),
   addPost: state => dispatch(addPost(state)),
-  deletePost: id => dispatch(deletePost(id))
+  deletePost: id => dispatch(deletePost(id)),
+  updatePost: state => dispatch(updatePost(state))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostsContainer)
