82b208ee2687ae4906d6dec2e56261b3e43cc0b8
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 10:31:02 -0400

    Define case statements for ADD_POST and DELETE_POST actions

--- client/src/reducers/posts_reducer.js
+++ client/src/reducers/posts_reducer.js
@@ -13,6 +13,16 @@ export default function postsReducer(state = {
     case 'FETCH_POSTS':
       return { loading: false, posts: action.payload }
 
+    case 'ADD_POST':
+      const post = { id: cuidFn(), title: action.title, content: action.content, author: action.author, comments: [] }
+      return { ...state, posts: [...state.posts, post]}
+
+    case 'DELETE_POST':
+      return {
+        ...state,
+        posts: state.posts.filter(post => post.id !== action.id)
+      }
+
     default:
       return state;
   }
