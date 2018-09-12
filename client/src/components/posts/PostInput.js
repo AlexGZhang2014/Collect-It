c5d348e5a5c7f9d2a66b740a2f7538a60ff98266
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 10:04:07 -0400

    Create PostInput component with most of its functionality

---
+++ client/src/components/posts/PostInput.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+class PostInput extends Component {
+  state = {
+    title: '',
+    content: '',
+    author: ''
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      title: '',
+      content: '',
+      author: ''
+    });
+  }
+
+  render() {
+    return (
+      <div className="post-form" onSubmit={this.handleSubmit}>
+        <form>
+          <h2>Create a new post here!</h2>
+          <br />
+          <label>Your Post Title:</label>
+          <br />
+          <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
+          <br />
+          <label>Your Name:</label>
+          <br />
+          <input type="text" name="author" value={this.state.author} onChange={this.handleChange} />
+          <br />
+          <label>Your Post Content:</label>
+          <br />
+          <textarea name="content" value={this.state.content} onChange={this.handleChange} />
+          <br />
+          <input type="submit" value="Create post"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default PostInput
