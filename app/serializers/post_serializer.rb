844dd8f622ccd3f0dba0f453e8d630ac789904d0
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-12 01:49:16 -0400

    Add created_at and updated_at attributes to Post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,4 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :author
+  attributes :id, :title, :content, :author, :created_at, :updated_at
   has_many :comments
 end
