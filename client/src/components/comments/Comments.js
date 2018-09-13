cb92ee10aa1a82c874be2df00a8a91d24a85c6f0
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-13 19:01:29 -0400

    Refactor so that it can display either the EditCommentForm or Comment component

--- client/src/components/comments/Comments.js
+++ client/src/components/comments/Comments.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
 import Comment from './Comment'
+import EditCommentForm from './EditCommentForm'
 
 class Comments extends Component {
   render() {
-    const comments = this.props.comments.map(comment => comment.post && this.props.post && comment.post.id === this.props.post.id ? <Comment key={comment.id} comment={comment} deleteComment={this.props.deleteComment} /> : null)
-
     return (
-      <div className="comments">{ comments }</div>
+      <div className="comments">
+        {this.props.comments.map(comment => {
+          if (comment.post && this.props.post && comment.post.id === this.props.post.id) {
+            if (this.props.editCommentId === comment.id) {
+              return (
+                <EditCommentForm
+                  comment={comment}
+                  post={this.props.post}
+                  key={comment.id}
+                  updateComment={this.props.updateComment}
+                  toggleEditOff={this.props.toggleEditOff}
+                  />
+              );
+            } else {
+              return (
+                <Comment
+                  key={comment.id}
+                  comment={comment}
+                  deleteComment={this.props.deleteComment}
+                  toggleEditOn={this.props.toggleEditOn}
+                  />
+              );
+            }
+          }
+        return null;})
+        }
+      </div>
     )
   }
 }
