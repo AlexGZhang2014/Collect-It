a0b17189896cf700bcf0f9e05f076122af649b7d
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-13 19:02:24 -0400

    Add button for editing and text that shows when comment was last updated

--- client/src/components/comments/Comment.js
+++ client/src/components/comments/Comment.js
@@ -6,8 +6,10 @@ const Comment = props => {
   return (
     <div className="comment">
       <h4>{props.comment.author} wrote: (<Moment date={props.comment.created_at} fromNow />)</h4>
-      <button onClick={() => props.deleteComment(props.comment.id)}>Delete this comment</button>
       <p>{props.comment.content}</p>
+      <h6>Last updated: <Moment date={props.comment.updated_at} fromNow /></h6>
+      <button onClick={() => props.toggleEditOn(props.comment.id)}>Edit this comment</button>
+      <button onClick={() => props.deleteComment(props.comment.id)}>Delete this comment</button>
     </div>
   )
 }
