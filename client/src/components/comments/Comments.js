8410813d129ea2bd2d376c1d4162abbffdf8aabc
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-13 17:15:10 -0400

    Pass down deleteComment thru props

--- client/src/components/comments/Comments.js
+++ client/src/components/comments/Comments.js
@@ -3,7 +3,7 @@ import Comment from './Comment'
 
 class Comments extends Component {
   render() {
-    const comments = this.props.comments.map(comment => comment.post && this.props.post && comment.post.id === this.props.post.id ? <Comment key={comment.id} comment={comment} /> : null)
+    const comments = this.props.comments.map(comment => comment.post && this.props.post && comment.post.id === this.props.post.id ? <Comment key={comment.id} comment={comment} deleteComment={this.props.deleteComment} /> : null)
 
     return (
       <div className="comments">{ comments }</div>
