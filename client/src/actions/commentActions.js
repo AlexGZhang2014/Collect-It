d57ee4a9c6f3a17c3b7068f91f975657cd5c9f74
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-13 16:25:10 -0400

    Define addComment function

--- client/src/actions/commentActions.js
+++ client/src/actions/commentActions.js
@@ -9,3 +9,31 @@ export function fetchComments() {
       }));
   }
 }
+
+export function addComment(state) {
+  return dispatch => {
+    dispatch({ type: 'ADDING_COMMENT' });
+
+    const commentData = {
+      comment: {
+        content: state.content,
+        author: state.author,
+        post_id: state.postId
+      }
+    }
+
+    fetch('/api/v1/comments', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(commentData)
+    })
+      .then(response => response.json())
+      .then(commentJSON => dispatch({
+        type: "ADDED_COMMENT",
+        id: commentJSON.id,
+        content: commentJSON.content,
+        author: commentJSON.author,
+        post: commentJSON.post
+      }));
+  }
+}
