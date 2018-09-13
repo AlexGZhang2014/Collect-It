0fc81b7da3cb11ae7868bc2f8faba8e03b9e0079
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-13 17:15:30 -0400

    Add button for deleting comment

--- client/src/components/comments/Comment.js
+++ client/src/components/comments/Comment.js
@@ -6,6 +6,7 @@ const Comment = props => {
   return (
     <div className="comment">
       <h4>{props.comment.author} wrote: (<Moment date={props.comment.created_at} fromNow />)</h4>
+      <button onClick={() => props.deleteComment(props.comment.id)}>Delete this comment</button>
       <p>{props.comment.content}</p>
     </div>
   )
