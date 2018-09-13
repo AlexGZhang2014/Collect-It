b938a7fb3d9bce138c90778f49cd1bc9f508645a
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-13 15:35:08 -0400

    Add comments to state by adding commentsReducer to combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import postsReducer from './posts_reducer'
 import collectionsReducer from './collections_reducer'
+import commentsReducer from './comments_reducer'
 
 const rootReducer = combineReducers({
   posts: postsReducer,
-  collections: collectionsReducer
+  collections: collectionsReducer,
+  comments: commentsReducer
 })
 
 export default rootReducer
