c43751a2f3f179b2091f92b9a4534f4047fc984e
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-13 16:25:41 -0400

    Define case for ADDED_COMMENT

--- client/src/reducers/comments_reducer.js
+++ client/src/reducers/comments_reducer.js
@@ -9,6 +9,14 @@ export default function commentsReducer(state = {
     case 'FETCH_COMMENTS':
       return { loading: false, comments: action.payload }
 
+    case 'ADDED_COMMENT':
+      const comment = { id: action.id, content: action.content, author: action.author, post: action.post };
+
+      return {
+        ...state,
+        comments: [...state.comments, comment]
+      }
+
     default:
       return state;
   }
