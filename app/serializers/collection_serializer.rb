1322286ffc66c10f76db596e9376f5ca969dd0c7
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-13 23:03:58 -0400

    Add attributes created_at and updated_at

--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -1,5 +1,5 @@
 class CollectionSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :owner
+  attributes :id, :name, :description, :owner, :created_at, :updated_at
   has_many :items
   has_many :reviews
 end
