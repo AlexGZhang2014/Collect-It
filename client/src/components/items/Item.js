8345730c4fa7214c2c44312acc70b9dd2656990d
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 18:18:27 -0400

    Modify to display when item was created

--- client/src/components/items/Item.js
+++ client/src/components/items/Item.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Moment from 'react-moment'
+import 'moment-timezone'
 
 const Item = props => {
   return (
     <div className="item">
-      <h4>{props.item.name}</h4>
+      <h4>{props.item.name} (Added: <Moment date={props.item.created_at} fromNow />)</h4>
       <p>{props.item.description}</p>
     </div>
   )
