67ba524f438ee5bb4c90ae53639eed55bbb65178
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 18:10:55 -0400

    Connect to redux store, define mapStateToProps and mapDispatchToProps, and set up componentDidMount

--- client/src/containers/ItemsContainer.js
+++ client/src/containers/ItemsContainer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Items from '../components/items/Items'
+import { fetchItems } from '../actions/itemActions'
+import { connect } from 'react-redux'
 
 class ItemsContainer extends Component {
   trueStatement = (statement) => {
@@ -8,15 +10,30 @@ class ItemsContainer extends Component {
     }
   }
 
+  componentDidMount() {
+    this.props.fetchItems()
+  }
+
   render() {
     return (
       <div className="items-container">
         <h3>Items:</h3>
         {this.trueStatement(true)}
-        <Items items={this.props.collection.items}/>
+        <Items
+          items={this.props.items}
+          collection={this.props.collection}
+          />
       </div>
     )
   }
 }
 
-export default ItemsContainer
+const mapStateToProps = state => ({
+  items: state.items.items
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchItems: () => dispatch(fetchItems())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemsContainer)
