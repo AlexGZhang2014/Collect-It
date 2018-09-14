de117bccc226bb88b05b982a6b00505ea96b007a
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 15:46:27 -0400

    Define case for ADDED_REVIEW

--- client/src/reducers/reviews_reducer.js
+++ client/src/reducers/reviews_reducer.js
@@ -9,6 +9,14 @@ export default function reviewsReducer(state = {
     case 'FETCH_REVIEWS':
       return { loading: false, reviews: action.payload }
 
+    case 'ADDED_REVIEW':
+      const review = { id: action.id, title: action.title, content: action.content, author: action.author, rating: action.rating, collection: action.collection };
+
+      return {
+        ...state,
+        reviews: [...state.reviews, review]
+      }
+
     default:
       return state;
   }
