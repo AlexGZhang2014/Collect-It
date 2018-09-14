cd9163d7be1f2f030ff494d4ea1dadd12486c742
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 17:30:00 -0400

    Refactor so that EditReviewForm will display when the edit review button is clicked

--- client/src/components/reviews/Reviews.js
+++ client/src/components/reviews/Reviews.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Review from './Review'
+import EditReviewForm from './EditReviewForm'
 
 class Reviews extends Component {
   render() {
@@ -7,15 +8,29 @@ class Reviews extends Component {
       <div className="reviews">
         {this.props.reviews.map(review => {
           if (review.collection && this.props.collection && review.collection.id === this.props.collection.id) {
-            return (
-              <Review
-                key={review.id}
-                review={review}
-                deleteReview={this.props.deleteReview}
-                />
-            )
+            if (this.props.editReviewId === review.id) {
+              return (
+                <EditReviewForm
+                  review={review}
+                  collection={this.props.collection}
+                  key={review.id}
+                  updateReview={this.props.updateReview}
+                  toggleEditOff={this.props.toggleEditOff}
+                  />
+              );
+            } else {
+              return (
+                <Review
+                  key={review.id}
+                  review={review}
+                  deleteReview={this.props.deleteReview}
+                  toggleEditOn={this.props.toggleEditOn}
+                  />
+              );
+            }
           }
-        })}
+        return null;})
+        }
       </div>
     )
   }
