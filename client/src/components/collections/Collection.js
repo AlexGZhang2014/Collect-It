17adbf4b5ed304bcf784633969f76f7e9039bdc6
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-13 22:58:32 -0400

    Add created_at and updated_at to serializer and use Moment.js to render collection creation time

--- client/src/components/collections/Collection.js
+++ client/src/components/collections/Collection.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import ItemsContainer from '../../containers/ItemsContainer'
 import ReviewsContainer from '../../containers/ReviewsContainer.js'
+import Moment from 'react-moment'
+import 'moment-timezone'
 
 class Collection extends Component {
   render() {
@@ -9,7 +11,7 @@ class Collection extends Component {
     return (
       <div className="collection">
         <h2>{collection.name}</h2>
-        <h4>Owner: {collection.owner}</h4>
+        <h4>Owner: {collection.owner} (Created <Moment date={collection.created_at} fromNow />)</h4>
         <p>{collection.description}</p>
         <button onClick={() => this.props.deleteCollection(collection.id)}>Delete this collection</button>
         <ItemsContainer collection={collection} />
