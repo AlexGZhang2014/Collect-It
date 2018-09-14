c9b60da326663d7f7b144921430c77d33d94ae4e
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 17:09:55 -0400

    Define case for UPDATED_REVIEW

--- client/src/reducers/reviews_reducer.js
+++ client/src/reducers/reviews_reducer.js
@@ -23,6 +23,26 @@ export default function reviewsReducer(state = {
         reviews: state.reviews.filter(review => review.id !== action.id)
       }
 
+    case 'UPDATED_REVIEW':
+      const editedReview = {
+        id: action.id,
+        title: action.title,
+        content: action.content,
+        author: action.author,
+        rating: action.rating,
+        collection: action.collection
+      };
+
+      let newState = {
+        ...state,
+        reviews: state.reviews.map(review => {
+          if (review.id !== action.id) {
+            return review;
+          }
+          return editedReview;
+        })
+      };
+
     default:
       return state;
   }
