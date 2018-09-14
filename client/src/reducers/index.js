6531cabe78f09e451fdbbc471e9b8ba372873168
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 18:05:37 -0400

    Add itemReducer to combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,12 +3,14 @@ import postsReducer from './posts_reducer'
 import collectionsReducer from './collections_reducer'
 import commentsReducer from './comments_reducer'
 import reviewsReducer from './reviews_reducer'
+import itemsReducer from './items_reducer'
 
 const rootReducer = combineReducers({
   posts: postsReducer,
   collections: collectionsReducer,
   comments: commentsReducer,
-  reviews: reviewsReducer
+  reviews: reviewsReducer,
+  items: itemsReducer
 })
 
 export default rootReducer
