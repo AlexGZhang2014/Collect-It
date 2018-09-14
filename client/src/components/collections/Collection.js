3feefe8f8444b1c26b787aeacefc59873dd1dfa5
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 16:20:28 -0400

    Refactor so that addReview button is displayed and can toggle to the AddReviewForm. Also confirm that creating a new review works.

--- client/src/components/collections/Collection.js
+++ client/src/components/collections/Collection.js
@@ -3,10 +3,26 @@ import ItemsContainer from '../../containers/ItemsContainer'
 import ReviewsContainer from '../../containers/ReviewsContainer.js'
 import Moment from 'react-moment'
 import 'moment-timezone'
+import AddReviewForm from '../reviews/AddReviewForm'
 
 class Collection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addReviewStatus: false
+    }
+  }
+
+  toggleAddReview = () => {
+    this.setState({
+      addReviewStatus: !this.state.addReviewStatus
+    })
+  }
+
   render() {
     const collection = this.props.collection
+
+    const buttonOrForm = () => !this.state.addReviewStatus ? <button onClick={this.toggleAddReview}>Add a review</button> : <AddReviewForm collection={collection} toggleAddReview={this.toggleAddReview} addReview={this.props.addReview}/>
 
     return (
       <div className="collection">
@@ -16,6 +32,7 @@ class Collection extends Component {
         <h6>Last updated: <Moment date={collection.updated_at} fromNow /></h6>
         <button onClick={() => this.props.toggleEditOn(collection.id)}>Edit this collection</button>
         <button onClick={() => this.props.deleteCollection(collection.id)}>Delete this collection</button>
+        {buttonOrForm()}
         <ItemsContainer collection={collection} />
         <ReviewsContainer collection={collection} />
       </div>
