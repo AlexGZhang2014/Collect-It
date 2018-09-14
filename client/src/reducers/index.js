17abaeab79a64b42f108015ea4e634f67c7245e0
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 15:21:32 -0400

    Add missing import

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import postsReducer from './posts_reducer'
 import collectionsReducer from './collections_reducer'
 import commentsReducer from './comments_reducer'
+import reviewsReducer from './reviews_reducer'
 
 const rootReducer = combineReducers({
   posts: postsReducer,
