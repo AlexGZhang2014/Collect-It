8bf9c92e9ec17bf669da95a539c1f6ca6622ec91
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 18:22:25 -0400

    Define addItem function

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -9,3 +9,31 @@ export function fetchItems() {
       }));
   }
 }
+
+export function addItem(state) {
+  return dispatch => {
+    dispatch({ type: 'ADDING_ITEM' });
+
+    const itemData = {
+      item: {
+        name: state.name,
+        description: state.description,
+        collection_id: state.collectionId
+      }
+    }
+
+    fetch('/api/v1/items', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(itemData)
+    })
+      .then(response => response.json())
+      .then(itemJSON => dispatch({
+        type: "ADDED_ITEM",
+        id: itemJSON.id,
+        name: itemJSON.name,
+        description: itemJSON.description,
+        collection: itemJSON.collection
+      }));
+  }
+}
