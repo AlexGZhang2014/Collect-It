25fe78b39c1b11e5e92af594344576c4ee5bfbdf
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 15:36:39 -0400

    Define addReview function

--- client/src/actions/reviewActions.js
+++ client/src/actions/reviewActions.js
@@ -9,3 +9,33 @@ export function fetchReviews() {
       }));
   }
 }
+
+export function addReview(state) {
+  return dispatch => {
+    dispatch({ type: 'ADDING_REVIEW' });
+
+    const reviewData = {
+      review: {
+        title: state.title,
+        content: state.content,
+        author: state.author,
+        collection_id: state.collectionId
+      }
+    }
+
+    fetch('/api/v1/reviews', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(reviewData)
+    })
+      .then(response => response.json())
+      .then(reviewJSON => dispatch({
+        type: "ADDED_REVIEW",
+        id: reviewJSON.id,
+        title: reviewJSON.title,
+        content: reviewJSON.content,
+        author: reviewJSON.author,
+        collection: reviewJSON.collection
+      }));
+  }
+}
