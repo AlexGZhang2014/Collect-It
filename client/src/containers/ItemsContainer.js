28886325cfe1d0276eee8ac48d98bf318d52008a
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 19:40:23 -0400

    Define a bunch of functions that work together to correctly render the button for adding items and the AddItemForm component when necessary

--- client/src/containers/ItemsContainer.js
+++ client/src/containers/ItemsContainer.js
@@ -1,12 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Items from '../components/items/Items'
-import { fetchItems } from '../actions/itemActions'
+import { fetchItems, addItem } from '../actions/itemActions'
+import AddItemForm from '../components/items/AddItemForm'
 import { connect } from 'react-redux'
 
 class ItemsContainer extends Component {
-  toggleAddItemButton = (editCollectionId) => {
-    if (editCollectionId) {
-      return <button>Add Item</button>
+  constructor(props) {
+    super(props);
+    this.state = {
+      addItemStatus: false
+    }
+  }
+
+  toggleAddItem = () => {
+    this.setState({
+      addItemStatus: !this.state.addItemStatus
+    });
+    this.renderAddItemForm();
+  }
+
+  renderAddItemForm = () => {
+    if (this.state.addItemStatus) {
+      return <AddItemForm
+        collection={this.props.collection}
+        addItem={this.props.addItem}
+        toggleAddItem={this.toggleAddItem}
+        />
+    } else {
+      return null;
+    }
+  }
+
+  toggleAddItemButton = () => {
+    if (this.props.editCollectionId) {
+      return (
+        <Fragment>
+          <button onClick={this.toggleAddItem}>Add a new item</button>
+          <p>Click "Update collection" when you are done adding and updating items!</p>
+        </Fragment>
+      )
+    } else {
+      return null;
     }
   }
 
@@ -18,7 +52,8 @@ class ItemsContainer extends Component {
     return (
       <div className="items-container">
         <h3>Items:</h3>
-        {this.toggleAddItemButton(this.props.editCollectionId)}
+        {this.toggleAddItemButton()}
+        {this.renderAddItemForm()}
         <Items
           items={this.props.items}
           collection={this.props.collection}
@@ -33,7 +68,8 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  fetchItems: () => dispatch(fetchItems())
+  fetchItems: () => dispatch(fetchItems()),
+  addItem: state => dispatch(addItem(state))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ItemsContainer)
