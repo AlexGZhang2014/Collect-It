64a862917005cbff2acc10ba4bac099da7b6f66c
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-13 23:22:30 -0400

    Refactor so that either EditCollectionForm or Collection are displayed

--- client/src/components/collections/Collections.js
+++ client/src/components/collections/Collections.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 import Collection from './Collection'
+import EditCollectionForm from './EditCollectionForm'
 
 class Collections extends Component {
   render() {
-    const collections = this.props.collections.map(collection => <Collection key={collection.id} collection={collection} deleteCollection={this.props.deleteCollection} updateCollection={this.props.updateCollection} />)
-
     return (
       <div className="collections">
-        {collections}
+        {this.props.collections.map(collection => {
+          if (this.props.editCollectionId === collection.id) {
+            return (
+              <EditCollectionForm
+                collection={collection}
+                key={collection.id}
+                updateCollection={this.props.updateCollection}
+                />
+            )
+          } else {
+            return (
+              <Collection
+                key={collection.id}
+                collection={collection} deleteCollection={this.props.deleteCollection}
+                updateCollection={this.props.updateCollection}
+                />
+            )
+          }
+        })}
       </div>
     )
   }
