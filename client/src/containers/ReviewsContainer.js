9476fe093527553c7136f2be0874c469e72ca3c2
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 15:33:01 -0400

    Define mapStateToProps and mapDispatchToProps and connect to redux store

--- client/src/containers/ReviewsContainer.js
+++ client/src/containers/ReviewsContainer.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
 import Reviews from '../components/reviews/Reviews'
+import { fetchReviews } from '../actions/reviewActions'
+import { connect } from 'react-redux'
 
 class ReviewsContainer extends Component {
+
+  componentDidMount() {
+    this.props.fetchReviews()
+  }
+
   render() {
     return (
       <div className="reviews-container">
         <h3>Reviews:</h3>
-        <Reviews reviews={this.props.collection.reviews}/>
+        <Reviews
+          reviews={this.props.reviews}
+          collection={this.props.collection}
+          />
       </div>
     )
   }
 }
 
-export default ReviewsContainer
+const mapStateToProps = state => ({
+  reviews: state.reviews.reviews
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchReviews: () => dispatch(fetchReviews())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewsContainer)
