4372b072890bcaa063fa38ca198b23cfe835af1d
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-14 20:11:22 -0400

    Define function that renders delete button only when editCollectionId exists

--- client/src/components/items/Item.js
+++ client/src/components/items/Item.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Moment from 'react-moment'
 import 'moment-timezone'
 
-const Item = props => {
-  return (
-    <div className="item">
-      <h4>{props.item.name} (Added: <Moment date={props.item.created_at} fromNow />)</h4>
-      <p>{props.item.description}</p>
-    </div>
-  )
+class Item extends Component {
+  renderDeleteButton = () => {
+    if (this.props.editCollectionId) {
+      return (
+        <button onClick={() => this.props.deleteItem(this.props.item.id)}>Delete this item</button>
+      )
+    } else {
+      return null;
+    }
+  }
+
+  render() {
+    return (
+      <div className="item">
+        <h4>{this.props.item.name} (Added: <Moment date={this.props.item.created_at} fromNow />)</h4>
+        <p>{this.props.item.description}</p>
+        {this.renderDeleteButton()}
+      </div>
+    )
+  }
 }
 
 export default Item
