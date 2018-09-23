97af7c5d75dd3208527f264dbd7b92c6a9ea3874
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-23 18:27:58 -0400

    Modify reducer to include cases for actions that occur before fetch requests

--- client/src/reducers/items_reducer.js
+++ client/src/reducers/items_reducer.js
@@ -1,5 +1,8 @@
 export default function itemReducer(state = {
   loading: false,
+  adding: false,
+  deleting: false,
+  updating: false,
   items: []
 }, action) {
   switch (action.type) {
@@ -7,21 +10,32 @@ export default function itemReducer(state = {
       return { ...state, loading: true }
 
     case 'FETCH_ITEMS':
-      return { loading: false, items: action.payload }
+      return { ...state, loading: false, items: action.payload }
+
+    case 'ADDING_ITEM':
+      return { ...state, adding: true }
 
     case 'ADDED_ITEM':
       const item = { id: action.id, name: action.name, description: action.description, collection: action.collection };
 
       return {
         ...state,
+        adding: false,
         items: [...state.items, item]
       }
+
+    case 'DELETING_ITEM':
+      return { ...state, deleting: true }
 
     case 'DELETED_ITEM':
       return {
         ...state,
+        deleting: false,
         items: state.items.filter(item => item.id !== action.id)
       }
+
+    case 'UPDATING_ITEM':
+      return { ...state, updating: true }
 
     case 'UPDATED_ITEM':
       const editedItem = {
@@ -33,6 +47,7 @@ export default function itemReducer(state = {
 
       let newState = {
         ...state,
+        updating: false,
         items: state.items.map(item => {
           if (item.id !== action.id) {
             return item;
