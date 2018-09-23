b43cd19ca7c33914ed4d8e617379a0a3882d3b9b
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-23 18:31:50 -0400

    Modify reducer to include cases for actions that occur before fetch requests

--- client/src/reducers/reviews_reducer.js
+++ client/src/reducers/reviews_reducer.js
@@ -1,5 +1,8 @@
 export default function reviewsReducer(state = {
   loading: false,
+  adding: false,
+  deleting: false,
+  updating: false,
   reviews: []
 }, action) {
   switch (action.type) {
@@ -7,21 +10,32 @@ export default function reviewsReducer(state = {
       return { ...state, loading: true }
 
     case 'FETCH_REVIEWS':
-      return { loading: false, reviews: action.payload }
+      return { ...state, loading: false, reviews: action.payload }
+
+    case 'ADDING_REVIEW':
+      return { ...state, adding: true }
 
     case 'ADDED_REVIEW':
       const review = { id: action.id, title: action.title, content: action.content, author: action.author, rating: action.rating, collection: action.collection };
 
       return {
         ...state,
+        adding: false,
         reviews: [...state.reviews, review]
       }
+
+    case 'DELETING_REVIEW':
+      return { ...state, deleting: true }
 
     case 'DELETED_REVIEW':
       return {
         ...state,
+        deleting: false,
         reviews: state.reviews.filter(review => review.id !== action.id)
       }
+
+    case 'UPDATING_REVIEW':
+      return { ...state, updating: true }
 
     case 'UPDATED_REVIEW':
       const editedReview = {
@@ -35,6 +49,7 @@ export default function reviewsReducer(state = {
 
       let newState = {
         ...state,
+        updating: false,
         reviews: state.reviews.map(review => {
           if (review.id !== action.id) {
             return review;
