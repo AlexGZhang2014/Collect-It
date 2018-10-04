c51f4221b35902792b304e830e45d7477546018b
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-10-03 20:14:43 -0400

    Add config for react router

--- config/routes.rb
+++ config/routes.rb
@@ -11,3 +11,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  !request.xhr? && request.format.html?
+end
