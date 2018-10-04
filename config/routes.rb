3ceb991f6abe8c9dc70c945f4c657d67a7b45453
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-10-03 20:30:39 -0400

    Move code to the right place

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,8 @@ Rails.application.routes.draw do
       resources :likes
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
 
-get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-  !request.xhr? && request.format.html?
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
